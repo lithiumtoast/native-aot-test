9dfbaade37fac8b8dd67511c98e520a9382b0c0c
Lucas Girouard-Stranks <lustranks@gmail.com>
2023-01-18 22:07:37 -0500

    Works for Windows

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Runtime.InteropServices;
+
+[DllImport("_library")]
+static extern string pinvoke_get_platform_name();
+
+Console.WriteLine("Hello, World! " + pinvoke_get_platform_name());
